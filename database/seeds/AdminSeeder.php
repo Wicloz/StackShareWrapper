bc3150a70e1508d16f9e40070587e0601b84b78a
Wilco de Boer <wfh@wilcodeboer.me>
2017-05-08 11:56:49 +0200

    Add seeder for admin user data

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::where(DB::raw('1 = 1'))->delete();
+
+        if (!empty(env('ADMIN_EMAIL')) && !empty(env('ADMIN_PASSWORD'))) {
+            User::create([
+                'email' => env('ADMIN_EMAIL'),
+                'password' => bcrypt(env('ADMIN_PASSWORD')),
+            ]);
+        }
+    }
+}
