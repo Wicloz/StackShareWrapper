88299a80c08a210c2b70cd2749730fde476958e1
Wilco de Boer <wfh@wilcodeboer.me>
2017-05-09 00:11:43 +0200

    Basic folder page

--- resources/views/folder.blade.php
+++ resources/views/folder.blade.php
@@ -1,2 +1,37 @@
 @extends('layouts.app')
 @section('title', $folder->path)
+
+@section('content-center')
+    <div class="table-responsive">
+        <table class="table table-striped table-hover">
+
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($folder->subFolders as $subFolder)
+                    <tr>
+                        <td></td>
+                        <td>
+                            <a href="{{ url($subFolder->path) }}">{{ $subFolder->name }}</a>
+                        </td>
+                    </tr>
+                @endforeach
+
+                @foreach ($folder->subFiles as $subFile)
+                    <tr>
+                        <td></td>
+                        <td>
+                            <a href="{{ url($subFile->path) }}">{{ $subFile->name }}</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+    </div>
+@endsection
