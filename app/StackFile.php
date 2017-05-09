6a35ce49d7f9bf8a5f1736fe3200fd20dea67414
Wilco de Boer <wfh@wilcodeboer.me>
2017-05-10 01:32:19 +0200

    Basic file viewing

--- app/StackFile.php
+++ app/StackFile.php
@@ -18,4 +18,50 @@ class StackFile extends StackItem
     {
         return $this->belongsTo('App\StackFolder');
     }
+
+    /**
+ * @return bool
+ */
+    public function getCanPreviewAttribute()
+    {
+        return $this->mime_min === 'image' || $this->mime_min === 'video' || $this->mime_min === 'audio';
+    }
+
+    /**
+     * @return bool
+     */
+    public function getCanThumbnailAttribute()
+    {
+        return $this->mime_min === 'image';
+    }
+
+    /**
+     * @return string
+     */
+    public function getMimeMinAttribute()
+    {
+        return explode('/', $this->mimetype)[0];
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreviewThumbAttribute()
+    {
+        $baseurl = config('stack.baseurl');
+        $shareid = config('stack.shareid');
+
+        return $baseurl . '/public-share/' . $shareid . '/preview?path=' . $this->path . '&mode=thumbnail';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPreviewFullAttribute()
+    {
+        $baseurl = config('stack.baseurl');
+        $shareid = config('stack.shareid');
+
+        return $baseurl . '/public-share/' . $shareid . '/preview?path=' . $this->path . '&mode=full';
+    }
 }
--- resources/views/file.blade.php
+++ resources/views/file.blade.php
@@ -1,2 +1,12 @@
 @extends('layouts.app')
 @section('title', $item->path)
+
+@section('content-center')
+    @if ($item->mime_min === 'image')
+        <img src="{{ $item->preview_full }}" alt="{{ $item->name }}">
+    @elseif ($item->mime_min === 'video')
+        <video src="{{ $item->preview_full }}" autoplay>
+    @elseif ($item->mime_min === 'audio')
+        <audio src="{{ $item->preview_full }}" controls autoplay>
+    @endif
+@endsection
