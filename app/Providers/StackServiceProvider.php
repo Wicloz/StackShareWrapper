37e90cdc4056ebe32c8990af6f33672d7958818f
Wilco de Boer <wfh@wilcodeboer.me>
2017-05-14 17:25:20 +0200

    Fix the Stack service provider

--- app/Providers/StackServiceProvider.php
+++ app/Providers/StackServiceProvider.php
@@ -21,8 +21,20 @@ class StackServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        \App::bind(StackApi::class, function () {
+        $this->app->bind(StackApi::class, function () {
             return new StackApi(config('stack.baseurl'), config('stack.shareid'), config('stack.sharefolder'), config('stack.username'), config('stack.password'));
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            StackApi::class
+        ];
     }
 }
