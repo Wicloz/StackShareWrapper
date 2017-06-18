444f6db919dfdb9c30957ffdda28cfc294ff2fe5
Wilco de Boer <wfh@wilcodeboer.me>
2017-06-18 16:04:02 +0200

    Fix OCD

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/bootstrap.js', 'public/js')
-   .js('resources/assets/js/app.js', 'public/js')
+mix.js('resources/assets/js/app.js', 'public/js')
+   .js('resources/assets/js/bootstrap.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/bootstrap.scss', 'public/css')
    .copyDirectory('resources/assets/files', 'public');
