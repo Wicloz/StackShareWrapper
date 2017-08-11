f0c2c5394347697455e712c87597abb6427dfbd5
Wilco de Boer <wfh@wilcodeboer.me>
2017-08-11 17:50:36 +0200

    Revert "Move media files"
    This reverts commit f7694e2ed4c11ef1357ae0d3333079e2d860d3dd.

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,8 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .js('resources/assets/js/bootstrap.js', 'public/js')
    .js('resources/assets/js/pdfiframe.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/bootstrap.scss', 'public/css');
+   .sass('resources/assets/sass/bootstrap.scss', 'public/css')
+   .copyDirectory('resources/assets/files', 'public');
 
 if (mix.config.inProduction) {
     mix.version().disableNotifications();
